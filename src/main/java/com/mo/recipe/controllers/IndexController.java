53d375560f5fd5c06f2abd03eb02836a19b8bef0
Mohammed Khan <m.khan935@gmail.com>
2018-10-16 17:31:50 -0400

    added index Controller and index.html and setup thymleaf

---
+++ src/main/java/com/mo/recipe/controllers/IndexController.java
@@ -0,0 +1,11 @@
+package com.mo.recipe.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage(){ return  "index";}
+}
